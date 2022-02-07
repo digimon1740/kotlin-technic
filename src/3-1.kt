2c9fb45a054a0af9a47dbbb524a8ffafd80b3c86
sanghoon <digimon.1740@gmail.com>
2022-02-08 01:08:46 +0900

    3장 중간 정리

--- src/3-1.kt
+++ src/3-1.kt
@@ -1,25 +1,54 @@
-class Dd {
-    fun square(n: Int) = n * n
-    fun triple(n: Int) = n * 3
+fun main() {
+    { "Hello World!" }
 
     fun compose(f: (Int) -> Int, g: (Int) -> Int): (Int) -> Int = { f(g(it)) }
-
-    val compose2: ((Int) -> Int, (Int) -> Int) -> (Int) -> Int = { x, y ->
-        {
-            x(y(it))
-        }
-    }
-
-
-    val compose3: ((Double) -> (Double), (Double) -> Double) -> (Double) -> Double = { x, y-> { x(y(it)) } }
-
-    fun test() {
-
-        val value = compose(::square, ::triple)
-        val value2 = compose2(::square, ::triple)(1000)
-
-        val cosValue: Double = compose3({ x: Double -> Math.PI / 2 - x }, Math::sin)(2.0)
-
-    }
-
 }
+
+//
+//class Dd {
+//    fun square(n: Int) = n * n
+//    fun triple(n: Int) = n * 3
+//
+//    fun compose(f: (Int) -> Int, g: (Int) -> Int): (Int) -> Int = { f(g(it)) }
+//
+//    val compose2: ((Int) -> Int, (Int) -> Int) -> (Int) -> Int = { x, y ->
+//        {
+//            x(y(it))
+//        }
+//    }
+//
+//    val compose3: ((Double) -> (Double), (Double) -> Double) -> (Double) -> Double = { x, y -> { x(y(it)) } }
+//
+//    val compose4: ((Int) -> Int, (Int) -> Int) -> (Int) -> Int = { x, y ->
+//        { z ->
+//            x(y(z))
+//        }
+//    }
+//
+//    fun <T, U, V> higherCompose() =
+//        { f: (U) -> V ->
+//            { g: (T) -> U ->
+//                { x: T -> f(g(x)) }
+//            }
+//        }
+//
+//    fun <T, U, V> higherCompose2(): ((U) -> V) -> ((T) -> U) -> (T) -> V =
+//        { f ->
+//            { g ->
+//                { x -> f(g(x)) }
+//            }
+//
+//        }
+//
+//    fun cos(arg: Double) = compose3({ x: Double -> Math.PI / 2 - x }, Math::sin)(2.0)
+//    fun test() {
+//
+//          val value = compose(::square, ::triple)
+//
+//
+//        val cosValue: Double = compose3({ x: Double -> Math.PI / 2 - x }, Math::sin)(2.0)
+//        val runnable = Runnable { println("This runs in a runnable") }
+//
+//
+//    }
+//}
